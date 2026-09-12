023ff2c535741111587d90d458184bd352396667
gsuarezropa-star <gsuarezropa@gmail.com>
2026-09-12 10:14:15 -0500

    Fix: Remove optional analyzer from startup

--- next.config.js
+++ next.config.js
@@ -1,35 +1,26 @@
-let withBundleAnalyzer = (config) => config
-try {
-  withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true"
-  })
-} catch (e) {}
-
 const withPWA = require("next-pwa")({
   dest: "public"
 })
 
-module.exports = withBundleAnalyzer(
-  withPWA({
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost"
-        },
-        {
-          protocol: "http",
-          hostname: "127.0.0.1"
-        },
-        {
-          protocol: "https",
-          hostname: "**"
-        }
-      ]
-    },
-    experimental: {
-      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-    }
-  })
-)
+module.exports = withPWA({
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost"
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1"
+      },
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+    ]
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
+  }
+})
